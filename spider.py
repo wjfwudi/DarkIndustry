6c57863df333a513cbeb264839e9eadc35bac571
Lu <18234662105@163.com>
2022-10-22 11:53:41 +0800

    amend

--- spider.py
+++ spider.py
@@ -14,6 +14,7 @@ def htmlinfo_extraction(URL):
     }  # 头部信息
     ssl._create_default_https_context = ssl._create_unverified_context  # 关闭https的认证
     try:
+        access_flag = 1  # 网页是否可访问
         request = urllib.request.Request(url,headers=header)
         reponse = urllib.request.urlopen(request).read()
 
@@ -28,7 +29,9 @@ def htmlinfo_extraction(URL):
         linkstr = ' '
         for link in soup.find_all('a'):
             all_link = link.get('href')
-            absolute_link = re.findall(pattern, str(all_link))
+            if all_link != None and len(all_link) != 0 and (all_link[0] != 'h' or all_link[1] != 't' or all_link[2] != 't' or all_link[3] != 'p') and all_link[0] == '/':   # 将网页的相对链接补充完整
+                all_link = url + all_link
+            absolute_link = re.findall(pattern, str(all_link))     # 将非网页链接剔除出去
             if len(absolute_link) != 0:
                 linklist.append(absolute_link[0])
                 linkstr = linkstr + absolute_link[0] + ' '
@@ -38,6 +41,8 @@ def htmlinfo_extraction(URL):
         picstr = ' '
         for pic in soup.find_all('img'):
             all_pic = pic.get('src')
+            if all_pic != None and len(all_pic) != 0 and (all_pic[0] != 'h' or all_pic[1] != 't' or all_pic[2] != 't' or all_pic[3] != 'p') and all_pic[0] == '/' :   # 将网页的图片相对链接补充完整
+                all_pic = url + all_pic
             absolute_pic = re.findall(pattern, str(all_pic))
             if len(absolute_pic) != 0:
                 piclist.append(absolute_pic[0])
@@ -48,9 +53,14 @@ def htmlinfo_extraction(URL):
         scriptstr = ' '
         for script in soup.find_all('script'):
             javascript = script.get('src')
-            if javascript != None:
-                scriptlist.append(javascript)
-                scriptstr = scriptstr + javascript + ' '
+            if javascript != None and len(javascript) != 0 and (javascript[0] != 'h' or javascript[1] != 't' or javascript[2] != 't' or javascript[3] != 'p') and javascript[0] == '/':   # 将网页的脚本相对链接补充完整:
+                javascript = url + javascript
+            absolute_javascript = re.findall(pattern, str(javascript))
+            if len(absolute_javascript) != 0:
+                scriptlist.append(absolute_javascript[0])
+                scriptstr = picstr + absolute_javascript[0] + ' '
+
+
 
         # 提取页面关键词
         keyword = ''
@@ -59,11 +69,9 @@ def htmlinfo_extraction(URL):
                 keyword = meta.get('content')
 
         # 提取网页所有文字内容
-        html_text = ''
         html_text = soup.get_text()
 
         # 提取页面标题
-        html_title = ''
         if soup.title is not None:
             html_title = soup.title.string
         else:
@@ -77,6 +85,7 @@ def htmlinfo_extraction(URL):
         # print(soup.a)
 
     except urllib.error.HTTPError as e:
+        access_flag = 0  # 网页不可访问
         linklist = None
         linkstr = None
         piclist = None
@@ -88,10 +97,9 @@ def htmlinfo_extraction(URL):
         html_title = None
 
 
+    return linklist,linkstr,piclist,picstr,scriptlist,scriptstr,keyword,html_text,html_title,access_flag
 
 
-    return linklist,linkstr,piclist,picstr,scriptlist,scriptstr,keyword,html_text,html_title
-
-
-# url = 'http://baidu.com'  # 菜鸟教程搜索页面
-# linklist,linkstr,piclist,picstr,scriptlist,scriptstr,keyword,html_text,html_title = htmlinfo_extraction(url)
+# url = 'http://douban.com'  # 要爬取的url页面
+# linklist,linkstr,piclist,picstr,scriptlist,scriptstr,keyword,html_text,html_title,access_flag = htmlinfo_extraction(url)
+# print(scriptlist)
